ffbb670433b43fba3f1ae62551b0922b73a8a728
Vanessa Maldonado <vanessa.rodriguez3@mavs.uta.edu>
2022-05-21 23:57:53 -0500

    displaying weather data and fetching 5 day forecast data

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,22 @@
+// DOM elements for seach bar 
 var citySearchEl = document.getElementById('city')
 var cityFormEl = document.querySelector('#city-form')
 var searchBtn = document.getElementById('search-btn')
 var dayForecastContainer = document.querySelector('#day-forecast')
 
-var temperature = document.querySelector(".temp");
+// DOM elements to display weather
+var icon = document.querySelector(".icon")
+var city = document.querySelector(".city-name")
+var date = document.querySelector(".date");
+var temperature = document.querySelector(".temp")
+var wind = document.querySelector(".wind")
+var humidity = document.querySelector(".humidity")
 const kelvin = 273;
 
 var weatherArray = [];
 
 
-// TESTING THIS CODE WORKS!!!!
+// function to get city input from user
 var formSubmitHandler = function(event) {
   event.preventDefault();
   // get value from input element
@@ -17,12 +24,15 @@ var formSubmitHandler = function(event) {
   console.log(city);
   if (city) {
       getWeather(city);
+      getForecast(city);
       citySearchEl.value= "";
+    // alert user if there input in invalid
   } else {
       alert("Please enter a valid city");
   }
 };
 
+// function to fetch api data and display daily forecast
 var getWeather = function(city) {
   // fetch api with user city search
   var apiUrl = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=d7e25feadbb98d58fea6663edfb99b38";
@@ -34,11 +44,24 @@ var getWeather = function(city) {
       })
     .then((data) => {
         console.log(data);
+        // display city name
+        city.textContent = data.name;
+
+        //display date 
+
+
+        //display weather icon
+       // icon.innerHTML = data.weather[0].icon; 
+
         // display temp in farenheit
         temperature.textContent =  Math.floor(data.main.temp - kelvin)* 1.8 + 32 + "°F";
-        summary.textContent = data.weather[0].description;
-        loc.textContent = data.name + "," + data.sys.country;
-        let icon1 = data.weather[0].icon; 
+
+        // display wind 
+        wind.textContent = data.wind.speed + " mph";
+
+        // display humidity
+        humidity.textContent = data.main.humidity;
+        
     });  
         // alert if there is an error
         //} else {
@@ -55,7 +78,7 @@ var getWeather = function(city) {
 // click event
 searchBtn.addEventListener("click", formSubmitHandler);
 
-
+/*
 // function to display weather info 
 var displayWeather = function() {
   // check if api returned weather info
@@ -64,4 +87,19 @@ var displayWeather = function() {
     return;
   };
 }
-     
+*/
+
+// get 5 day forecast
+var getForecast = function(city) {
+    var forecastUrl = "http://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=d7e25feadbb98d58fea6663edfb99b38";
+
+    fetch(forecastUrl)
+        .then((response) => {
+        return response.json();
+      })
+        .then((data) => {
+        console.log(data);
+})
+};
+
+
